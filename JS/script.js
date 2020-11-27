378ea2148885bc601ce79d0e2e41e5c8d0274d7c
siri2021 <62374777+siri2021@users.noreply.github.com>
2020-11-27 15:19:21 +0100

    correction

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+/*$(document).ready(function(){
+   const bars=document.querySelectorAll('.progress_bar'); 
+   bars.forEach(function(bar){
+let percentage=bar.dataset.percent;
+let tooltip= bar.children[0] ;
+tooltip.innerText=percentage + '%';
+bar.style.width=percentage + '%';
+console.log(percentage);
+   })
+}) */
+
+function displayText(){
+   document.getElementById("text").innerHTML = "I'm passionate about everything whether its coding or cooking..being involved 100% in what i do for the moment is my philosophy of life";
+//   $("#text").load("C:\Users\Siri\OneDrive\Desktop\Siri_portfolio/about.txt");
+}
